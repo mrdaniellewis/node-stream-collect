16c64431503ac9f92e1fabbfa7f05238e776c6e7
Daniel Lewis <github@mrdaniellewis.co.uk>
2015-01-27 23:52:04 +0000

    Inital commit

---
+++ index.js
@@ -0,0 +1,110 @@
+/* jshint node: true */
+"use strict";
+
+var stream = require('stream');
+var util = require('util');
+var promiseUtil = require('promise-util');
+var debug = require('debug')('stream-collect');
+
+/**
+ *	When a listener for an event named collect is 
+ *	added setup collecting the data.
+ */
+var addListener = function(name) {
+	
+	if ( name !== 'collect' ) {
+		return;
+	}
+
+	if ( this._collecting ) {
+		// Don't add more than once
+		return;
+	}
+
+	debug( 'adding collect method' );
+
+	var collected;
+	if ( this._readableState.objectMode ) {
+		collected = [];
+	} else if ( this._readableState.encoding === null ) {
+		collected = new Buffer(0);
+	} else {
+		collected = '';
+	}
+
+	this
+		.on( 'data', function(chunk) {
+			
+			debug( 'data', chunk );
+
+			if ( chunk !== null ) {
+				if ( this._readableState.objectMode ) {
+					collected.push(chunk);
+				} else if ( this._readableState.encoding === null ) {
+					collected = Buffer.concat( [ collected, chunk ] );
+				} else {
+					collected += chunk;
+				}
+			}
+		} )
+		.on( 'end', function() {
+			this.emit( 'collect', collected );
+		} );
+
+	this._collecting = true;
+};
+
+/**
+ *	Add the collect event to the stream
+ *	@param {Stream} stream
+ */
+function addToStream(stream) {
+
+	// Don't add more than once
+	if ( stream.listeners('addListener').indexOf(addListener) > -1 ) {
+		return;
+	}
+
+	stream.on( 'newListener', addListener );
+
+	return stream;
+}
+
+/**
+ *	Collect all data in a stream and return it in a callback
+ *	@param {Stream} stream A stream
+ *	@param {Function} cb Callback, the first argument will be the data
+ */
+function collect( stream, encoding, cb ) {
+
+	if ( typeof encoding === 'function' ) {
+		cb = encoding;
+		encoding = null;
+	}
+
+	var defer = promiseUtil.defer();
+
+	stream
+		.pipe( new PassThrough( { encoding: encoding } ) )
+		.on( 'collect', function(data) {
+			defer.resolve(data);
+			if (cb) {
+				cb( data );
+			}
+		} )
+		.on( 'error', defer.reject );
+
+	return defer;
+}
+
+/**
+ *	Returns a PassThrough stream augmented with collect
+ *	@param {Object} [options]
+ */
+function PassThrough(options) {
+	return addToStream( new stream.PassThrough(options) );
+}
+
+module.exports = collect;
+collect.addToStream = addToStream;
+collect.PassThrough = PassThrough;
---
+++ test/index.js
@@ -0,0 +1,167 @@
+/* jshint node: true, mocha: true */
+"use strict";
+
+
+var expect = require('expect');
+var collect = require('../');
+var fs = require('fs');
+var path = require('path');
+var PassThrough = require('stream').PassThrough;
+
+var testDataPart1 = 'Some data to ';
+var testDataPart2 = 'go into the stream';
+var completeData = 'Some data to go into the stream';
+
+describe( 'collect.PassThrough', function() {
+
+	it( 'collects buffer data', function(done) {
+		var stream = new collect.PassThrough()
+			.on( 'collect', function(data) {
+				expect( Buffer.isBuffer(data) ).toBe(true);
+				expect( data.toString() ).toBe( completeData );
+				done();
+			} );
+
+		stream.write(testDataPart1);
+		stream.end(testDataPart2);
+
+	} );
+
+	it( 'collects string data', function(done) {
+		var stream = new collect.PassThrough( {encoding: 'utf8'})
+			.on( 'collect', function(data) {
+				expect( typeof data ).toBe('string');
+				expect( data ).toBe( completeData );
+				done();
+			} );
+
+		stream.write(testDataPart1);
+		stream.end(testDataPart2);
+
+	} );
+
+	it( 'handles encoding correctly with string data', function(done) {
+		var stream = new collect.PassThrough( {encoding: 'base64'})
+			.on( 'collect', function(data) {
+				expect( typeof data ).toBe('string');
+				expect( new Buffer(data, 'base64').toString() ).toBe( completeData );
+				done();
+			} );
+
+		stream.write(testDataPart1);
+		stream.end(testDataPart2);
+
+	} );
+
+	it( 'collects object data', function(done) {
+		var stream = new collect.PassThrough( {objectMode: true})
+			.on( 'collect', function(data) {
+				expect( Array.isArray(data) ).toBe(true);
+				expect( data ).toEqual( [1,2,3] );
+				done();
+			} );
+
+		stream.write(1);
+		stream.write(2);
+		stream.end(3);
+
+	} );
+
+	it( 'collects a big arse file', function(done) {
+
+		// If debug is on this is a little slow
+		this.timeout( 5000 );
+
+		var file = path.resolve( __dirname, 'war-and-peace.txt' );
+		var fileContents = fs.readFileSync( 
+			file, 
+			{encoding: 'utf8'}
+		);
+
+		var stream = new collect.PassThrough( {encoding: 'utf8'} )
+			.on( 'collect', function(data) {
+				expect(data).toBe(fileContents);
+				done();
+			} );
+
+		fs.createReadStream( file, {encoding: 'utf8'} )
+			.pipe( stream );
+
+	} );
+
+	it( 'can handle having two collect listerers', function(done) {
+		var stream = new collect.PassThrough()
+			.on( 'collect', function(data) {
+				// This one will be called first
+				expect( Buffer.isBuffer(data) ).toBe(true);
+				expect( data.toString() ).toBe( completeData );
+			} )
+			.on( 'collect', function(data) {
+				// Then this one
+				expect( Buffer.isBuffer(data) ).toBe(true);
+				expect( data.toString() ).toBe( completeData );
+				done();
+			} );
+
+
+		stream.write(testDataPart1);
+		stream.end(testDataPart2);
+
+	} );
+
+
+
+} );
+
+
+describe( 'collect', function() {
+
+	it( 'collects using a callback', function(done) {
+		
+		var stream = new PassThrough();
+
+		collect( stream, function( data ) {
+			expect( Buffer.isBuffer(data) ).toBe(true);
+			expect( data.toString() ).toBe( completeData );
+			done();
+		} );
+	
+		stream.write(testDataPart1);
+		stream.end(testDataPart2);
+
+	} );
+
+	it( 'collects using a specified encoding', function(done) {
+		
+		var stream = new PassThrough();
+
+		collect( stream, 'hex', function( data ) {
+			expect( typeof data ).toBe('string');
+			expect( new Buffer(data,'hex').toString() ).toBe( completeData );
+			done();
+		} );
+	
+		stream.write(testDataPart1);
+		stream.end(testDataPart2);
+
+	} );
+
+	it( 'collects using a promise', function() {
+		
+
+		var stream = new PassThrough();
+
+		var ret = collect(stream)
+			.then( function(data) {
+				expect( Buffer.isBuffer(data) ).toBe(true);
+				expect( data.toString() ).toBe( completeData );
+			} );
+	
+		stream.write(testDataPart1);
+		stream.end(testDataPart2);
+
+		return ret;
+
+	} );
+
+} );
