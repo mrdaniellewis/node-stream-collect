68017aaf2df900ce56fc9d3a6dfbab2c3fea5cd3
Daniel Lewis <github@mrdaniellewis.co.uk>
2017-11-05 16:45:36 +0000

    update

--- index.js
+++ index.js
@@ -1,154 +1,146 @@
-/* jshint node: true */
-"use strict";
+'use strict';
 
-var stream = require('stream');
-var util = require('util');
-var promiseUtil = require('promise-util');
-var debug = require('debug')('stream-collect');
+const { PassThrough } = require('stream');
+const debug = require('debug')('stream-collect');
 
 /**
- *	When a listener for an event named collect is 
- *	added setup collecting the data.
+ *  When a listener for an event named collect is added start collecting the data
  */
-var addListener = function(name) {
-	
-	if ( name !== 'collect' ) {
-		return;
-	}
+const addListener = function addListener(name) {
+  if (name !== 'collect') {
+    return;
+  }
 
-	if ( this._collecting ) {
-		// Don't add more than once
-		return;
-	}
+  if (this._collecting) {
+    // Don't add more than once
+    return;
+  }
 
-	debug( 'adding collect method', this._readableState.objectMode, this._readableState.encoding );
+  debug('adding collect method', this._readableState.objectMode, this._readableState.encoding);
 
-	var collected;
-	if ( this._readableState.objectMode ) {
-		collected = [];
-	} else if ( this._readableState.encoding === null ) {
-		collected = new Buffer(0);
-	} else {
-		collected = '';
-	}
+  let collected;
+  if (this._readableState.objectMode) {
+    collected = [];
+  } else if (this._readableState.encoding === null) {
+    collected = Buffer.from('');
+  } else {
+    collected = '';
+  }
 
-	this
-		.on( 'data', function(chunk) {
-			
-			debug( 'data', chunk );
+  this
+    .on('readable', () => {
+      let chunk;
+      while ((chunk = this.read()) !== null) {
+        debug('data', chunk);
 
-			if ( chunk !== null ) {
-				if ( this._readableState.objectMode ) {
-					collected.push(chunk);
-				} else if ( this._readableState.encoding === null ) {
-					collected = Buffer.concat( [ collected, chunk ] );
-				} else {
-					collected += chunk;
-				}
-			}
-		} )
-		.on( 'end', function() {
-			this.emit( 'collect', collected );
-		} );
+        if (chunk !== null) {
+          if (this._readableState.objectMode) {
+            collected.push(chunk);
+          } else if (this._readableState.encoding === null) {
+            collected = Buffer.concat([collected, chunk]);
+          } else {
+            collected += chunk;
+          }
+        }
+      }
+    })
+    .on('end', () => this.emit('collect', collected));
 
-	this._collecting = true;
+  this._collecting = true;
 };
 
 /**
- *	Add the collect event to the stream
- *	@param {Stream} stream
+ *  Add the collect event to the stream
+ *  @param {Stream} stream
  */
 function addToStream(stream) {
+  // Don't add more than once
+  if (stream.listeners('addListener').includes(addListener)) {
+    return stream;
+  }
 
-	// Don't add more than once
-	if ( stream.listeners('addListener').indexOf(addListener) > -1 ) {
-		return;
-	}
+  stream.on('newListener', addListener);
 
-	stream.on( 'newListener', addListener );
-
-	return stream;
+  return stream;
 }
 
 /**
- *	Collect all data in a stream and return it in a callback
- *	@param {Stream} stream A stream
- *	@param {Function} cb Callback, the first argument will be the data
+ *  Returns a PassThrough stream with the collect event added and a collect method
  */
-function collect( stream, encoding, cb ) {
+class Collect extends PassThrough {
+  constructor(options) {
+    super(options);
+    addToStream(this);
+    this._collected = null;
 
-	if ( typeof encoding === 'function' ) {
-		cb = encoding;
-		encoding = null;
-	}
+    this.on('pipe', source => source.on('error', e => this.emit('error', e)));
+  }
 
-	var defer = promiseUtil.defer();
+  collect() {
+    if (!this._collected) {
+      this._collected = new Promise((resolve, reject) => {
+        this.on('collect', resolve);
+        this.on('error', reject);
+      });
+    }
+    return this._collected;
+  }
 
-	stream
-		.pipe( new PassThrough( { 
-			encoding: encoding, 
-			objectMode: stream._readableState.objectMode 
-		} ) )
-		.on( 'collect', function(data) {
-			defer.resolve(data);
-			if (cb) {
-				cb( data );
-			}
-		} )
-		.on( 'error', defer.reject );
+  /** @depreacted */
+  then(resolve, reject) {
+    return this.collect().then(resolve, reject);
+  }
 
-	return defer;
+  /** @depreacted */
+  catch(reject) {
+    return this.collect().then(null, reject);
+  }
 }
 
 /**
- *	Returns a PassThrough stream augmented with collect
- *	The PassThrough stream is also a thenable and can be used as a promise
- *	@param {Object} [options]
+ *  A CollectStreamObject set to objectMode
  */
-function PassThrough(options) {
-	
-	if ( !(this instanceof PassThrough ) ) {
-		return new PassThrough(options);
-	}
-
-	stream.PassThrough.call( this, options );
-	addToStream(this);
-	this._resolved = null;
-
-	this.on( 'pipe', function(source) { 
-		source.on( 'error', this.emit.bind( this, 'error' ) );
-	}.bind(this) );
+class CollectObjects extends Collect {
+  constructor(options = {}) {
+    options.objectMode = true;
+    super(options);
+  }
 }
-
-util.inherits( PassThrough, stream.PassThrough );
-
-PassThrough.prototype.then = function( resolve, reject ) {
-
-	if ( !this._resolved ) {
-		this._resolved = promiseUtil.defer();
-		this
-			.on( 'collect', this._resolved.resolve )
-			.on( 'error', this._resolved.reject );
-	}
-	return this._resolved.then( resolve, reject );
-};
-
-PassThrough.prototype.catch = function( reject ) {
-	
-	return this.then( null, reject );
-
-};
 
 /**
- *	A PassThrough set to objectMode
+ *  Collect all data in a stream and return as a promise or in a callback
+ *  @param {Stream} stream A stream
+ *  @param {String} [encoding] Stream encoding - optional
+ *  @param {Function} cb Callback, the first argument will be the data
+ *  @returns {Promise}
  */
-function PassThroughObject(options) {
-	options = options || {};
-	options.objectMode = true;
-	return new PassThrough(options);
+function collect(stream, encoding, cb = () => {}) {
+  if (typeof encoding === 'function') {
+    cb = encoding;
+    encoding = null;
+  }
+
+  return stream
+    .pipe(new Collect({ encoding, objectMode: stream._readableState.objectMode }))
+    .collect()
+    .then((data) => {
+      cb(null, data);
+      return data;
+    })
+    .catch((e) => {
+      cb(e);
+      throw e;
+    });
 }
+
 
 module.exports = collect;
 collect.addToStream = addToStream;
-collect.PassThrough = collect.stream = PassThrough;
-collect.PassThroughObject = collect.objectStream = PassThroughObject;
+collect.Collect = Collect;
+collect.CollectObjects = CollectObjects;
+
+// deprecated names
+collect.PassThrough = function (options) { return new Collect(options); };
+collect.stream = collect.PassThrough;
+collect.PassThroughObject = function (options) { return new CollectObjects(options); };
+collect.objectStream = collect.PassThroughObject;
---
+++ test.js
@@ -0,0 +1,324 @@
+/* eslint-env node, jest */
+
+'use strict';
+
+const collect = require('./');
+const fs = require('fs');
+const path = require('path');
+const { PassThrough } = require('stream');
+
+const testDataPart1 = 'Some data to ';
+const testDataPart2 = 'go into the stream';
+const completeData = 'Some data to go into the stream';
+
+describe('collect.Collect', () => {
+  describe('collect event', () => {
+    it('collects buffer data', (done) => {
+      const stream = new collect.Collect()
+        .on('collect', (data) => {
+          expect(Buffer.isBuffer(data)).toBe(true);
+          expect(data.toString()).toBe(completeData);
+          done();
+        });
+
+      stream.write(testDataPart1);
+      stream.end(testDataPart2);
+    });
+
+    it('collects string data', (done) => {
+      const stream = new collect.Collect({ encoding: 'utf8' })
+        .on('collect', (data) => {
+          expect(typeof data).toBe('string');
+          expect(data).toBe(completeData);
+          done();
+        });
+
+      stream.write(testDataPart1);
+      stream.end(testDataPart2);
+    });
+
+    it('handles encoding correctly with string data', (done) => {
+      const stream = new collect.Collect({ encoding: 'base64' })
+        .on('collect', (data) => {
+          expect(typeof data).toBe('string');
+          expect(Buffer.from(data, 'base64').toString()).toBe(completeData);
+          done();
+        });
+
+      stream.write(testDataPart1);
+      stream.end(testDataPart2);
+    });
+
+    it('collects object data', (done) => {
+      const stream = new collect.Collect({ objectMode: true })
+        .on('collect', (data) => {
+          expect(Array.isArray(data)).toBe(true);
+          expect(data).toEqual([1, 2, 3]);
+          done();
+        });
+
+      stream.write(1);
+      stream.write(2);
+      stream.end(3);
+    });
+
+    it('collects a big file', (done) => {
+      const file = path.resolve(__dirname, 'the-machine-stops.txt');
+      const fileContents = fs.readFileSync(
+        file,
+        { encoding: 'utf8' },
+      );
+
+      const stream = new collect.Collect({ encoding: 'utf8' })
+        .on('collect', (data) => {
+          expect(data).toBe(fileContents);
+          done();
+        });
+
+      fs.createReadStream(file, { encoding: 'utf8' })
+        .pipe(stream);
+    });
+
+    it('can handle having two collect listeners', (done) => {
+      const stream = new collect.Collect()
+        .on('collect', (data) => {
+          // This one will be called first
+          expect(Buffer.isBuffer(data)).toBe(true);
+          expect(data.toString()).toBe(completeData);
+        })
+        .on('collect', (data) => {
+          // Then this one
+          expect(Buffer.isBuffer(data)).toBe(true);
+          expect(data.toString()).toBe(completeData);
+          done();
+        });
+
+
+      stream.write(testDataPart1);
+      stream.end(testDataPart2);
+    });
+  });
+
+  describe('#collect', () => {
+    it('returns a promise resolving to the data', () => {
+      const stream = new collect.Collect();
+      stream.write(testDataPart1);
+      stream.end(testDataPart2);
+
+      return stream.collect((data) => {
+        expect(Buffer.isBuffer(data)).toBe(true);
+        expect(data.toString()).toBe(completeData);
+      });
+    });
+
+    it('returns the same promise for subsequent calls', () => {
+      const stream = new collect.Collect();
+      stream.write(testDataPart1);
+      stream.end(testDataPart2);
+
+      expect(stream.collect()).toEqual(stream.collect());
+    });
+
+    it('rejects if the stream errors', () => {
+      const stream = new collect.Collect();
+      const error = new Error('foo');
+
+      stream.write('foobar');
+
+      const promise = stream.collect();
+      stream.emit('error', error);
+
+      return promise
+        .then(() => { throw new Error('Should not have been called'); })
+        .catch(e => expect(e).toBe(error));
+    });
+
+    it('passes errors from a piped stream', () => {
+      const stream = new PassThrough();
+      const error = new Error('foo');
+
+      stream.write('foobar');
+
+      const promise = stream.pipe(new collect.Collect()).collect();
+      stream.emit('error', error);
+
+      return promise
+        .then(() => {
+          throw new Error('Should not have been called');
+        })
+        .catch((e) => {
+          expect(e).toBe(error);
+        });
+    });
+  });
+
+  describe('#then (deprecated)', () => {
+    it('returns a promise resolving to the data', () => {
+      const stream = new collect.Collect();
+      stream.write(testDataPart1);
+      stream.end(testDataPart2);
+
+      return stream.then((data) => {
+        expect(Buffer.isBuffer(data)).toBe(true);
+        expect(data.toString()).toBe(completeData);
+      });
+    });
+
+    it('rejects if the stream errors', () => {
+      const stream = new collect.Collect();
+      const error = new Error('foo');
+
+      stream.write('foobar');
+
+      const promise = stream
+        .then(
+          () => { throw new Error('Should not have been called'); },
+          e => expect(e).toBe(error),
+        );
+
+      stream.emit('error', error);
+
+      return promise;
+    });
+  });
+
+  describe('#catch (deprecated)', () => {
+    it('rejects if the stream errors', () => {
+      const stream = new collect.Collect();
+      const error = new Error('foo');
+
+      stream.write('foobar');
+
+      const promise = stream
+        .catch(e => expect(e).toBe(error));
+
+      stream.emit('error', error);
+
+      return promise;
+    });
+  });
+});
+
+describe('collect.PassThrough (deprecated)', () => {
+  it('creates a collect stream', () => {
+    expect(collect.PassThrough()).toBeInstanceOf(collect.Collect);
+  });
+
+  it('creates a collect stream when used with new', () => {
+    expect(new collect.PassThrough()).toBeInstanceOf(collect.Collect);
+  });
+});
+
+describe('collect.stream (deprecated)', () => {
+  it('creates a collect stream', () => {
+    expect(collect.stream()).toBeInstanceOf(collect.Collect);
+  });
+});
+
+describe('collect.CollectObjects', () => {
+  it('collects as an object stream', () => {
+    const stream = new collect.CollectObjects();
+    const ob1 = {};
+    const ob2 = {};
+    stream.write(ob1);
+    stream.end(ob2);
+
+    return stream.collect()
+      .then((data) => {
+        expect(data[0]).toBe(ob1);
+        expect(data[1]).toBe(ob2);
+      });
+  });
+});
+
+describe('collect.PassThroughObject (deprecated)', () => {
+  it('creates a collectobjects stream', () => {
+    expect(collect.PassThroughObject()).toBeInstanceOf(collect.CollectObjects);
+  });
+
+  it('creates a collect stream when used with new', () => {
+    expect(new collect.PassThroughObject()).toBeInstanceOf(collect.CollectObjects);
+  });
+});
+
+describe('collect.objectStream (deprecated)', () => {
+  it('creates a collectobjects stream', () => {
+    expect(collect.objectStream()).toBeInstanceOf(collect.CollectObjects);
+  });
+});
+
+describe('collect', () => {
+  it('collects using a callback', (done) => {
+    const stream = new PassThrough();
+
+    collect(stream, (error, data) => {
+      expect(Buffer.isBuffer(data)).toBe(true);
+      expect(data.toString()).toBe(completeData);
+      done();
+    });
+
+    stream.write(testDataPart1);
+    stream.end(testDataPart2);
+  });
+
+  it('collects using a specified encoding', (done) => {
+    const stream = new PassThrough();
+
+    collect(stream, 'hex', (error, data) => {
+      expect(typeof data).toBe('string');
+      expect(Buffer.from(data, 'hex').toString()).toBe(completeData);
+      done();
+    });
+
+    stream.write(testDataPart1);
+    stream.end(testDataPart2);
+  });
+
+  it('collects using a promise', () => {
+    const stream = new PassThrough();
+
+    const ret = collect(stream)
+      .then((data) => {
+        expect(Buffer.isBuffer(data)).toBe(true);
+        expect(data.toString()).toBe(completeData);
+      });
+
+    stream.write(testDataPart1);
+    stream.end(testDataPart2);
+
+    return ret;
+  });
+
+  it('collects object data', () => {
+    const stream = new PassThrough({ objectMode: true });
+
+    const ret = collect(stream);
+
+    stream.write(1);
+    stream.write(2);
+    stream.end(3);
+
+    return ret
+      .then((data) => {
+        expect(Array.isArray(data)).toBe(true);
+        expect(data).toEqual([1, 2, 3]);
+      });
+  });
+});
+
+describe('collect.addToStream', () => {
+  it('augments a stream with the collect event', (done) => {
+    const stream = new PassThrough();
+    expect(collect.addToStream(stream)).toEqual(stream);
+
+    stream
+      .on('collect', (data) => {
+        expect(data.toString()).toBe(completeData);
+        done();
+      });
+
+    stream.write(testDataPart1);
+    stream.end(testDataPart2);
+  });
+});
+
